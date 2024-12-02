cadf7914697d281618847a98ca045fc61875f85f
diego145xx <dracoxbox2@gmail.com>
2024-12-02 20:07:30 +0000

    corrigiendo

--- loginTask/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class TaskCreateForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=255)
-    content = forms.CharField(label='Content', widget=forms.Textarea())
